816da3ad1629adafd73a0014f97d326686c5fdec
Argus Aphocraphex <zelechos@hotmail.com>
2020-11-06 01:44:31 -0400

    Publicacion 0.0.1

--- JavaScript/ArrowFunctions.js
+++ JavaScript/ArrowFunctions.js
@@ -1,15 +1,11 @@
 /*
+Author: Pragmatic Coder
+Repositorio: https://github.com/Zelechos/Pragmatic_IMG
+
     The Arrow functions o Funciones Flecha
     Una expresión de función flecha es una alternativa 
     compacta a diferencia de la función tradicional, pero 
     es limitada y no se puede utilizar en todas las situaciones.
-
-    Diferencias y limitaciones:
-    No tiene sus propios enlaces a this o super.
-    No se debe usar como métodos.
-    No tiene argumentos o palabras clave new.target.
-    No se puede utilizar como constructor.
-    No se puede utilizar yield dentro de su cuerpo.
 */
 
 //Creamos una Funcion Bienvenida con varios mensajes
@@ -22,16 +18,17 @@ function Welcome(Callback){
     */
     Callback();
 }
+
+Welcome( ()=> console.log("Adios , dale LIKE a la Screnshot ") );
 /*  Aqui invocamos la Funcion Welcome
     Y dentro de ella creamos nuestra funcion flecha
     que servira como un callback que enviaremos
     a nuestra funcion Welcome
-    esta funcion flecha se denota con 
-    ParenTesis y una flecha :
-    () => |Esto es lo que la funcion retorna| 
-*/
-Welcome( ()=> console.log("Adios , dale LIKE a la Screnshot ") );
-/*
-Author: Pragmatic Coder
-Respositorio:
+    () => |Esto es lo que la funcion flecha retorna| 
+
+    {Lo que el Programa Muestra}
+    Hola, Bienvenido a Mi Instagram . . .           
+    Puedes seguirme en Twitter como @CoderPragmatic 
+    Y en GitHub https://github.com/Zelechos
+    Adios , dale LIKE a la Screnshot
 */
